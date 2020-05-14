39253888abcb660a025e3a631a24a33bb05e6b25
szymonSadowski <szymon.ssadows@gmail.com>
2020-05-14 11:12:39 +0200

    working gui navigation

---
+++ rehabGUI/app.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+import mainWindow
+
+LOCAL_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+
+class Main(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = mainWindow.Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    mainWindow = Main()
+    sys.exit(app.exec_())
